f9b11a9f13f1bf1c7c57cb13a4c4b8ee7b715f24
zm24acgRiyan <zm24acg@herts.ac.uk>
2025-12-08 18:08:06 +0000

    demo: add vulnerable dependency to trigger SCA gate

--- src/main/java/com/example/leandevsecops/LeanDevSecOpsApplication.java
+++ src/main/java/com/example/leandevsecops/LeanDevSecOpsApplication.java
@@ -1,13 +1,22 @@
-package com.example.leandevsecops; // <-- FIXED
+package com.example.leandevsecops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Import Log4j
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @SpringBootApplication
 public class LeanDevSecOpsApplication {
 
+	// Create a Log4j logger
+	private static final Logger logger = LogManager.getLogger(LeanDevSecOpsApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(LeanDevSecOpsApplication.class, args);
+
+		// Log a message to ensure Log4j is used
+		logger.info("Application started with vulnerable Log4j logger!");
 	}
-	
 }
